3bf068f2f4ac75c392bd688e0f8c730e87b6eb24
RodionovDenis <denisrodionov18@gmail.com>
2022-05-17 00:32:24 +0300

    final

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 from tf_explain.core.grad_cam import GradCAM
+import numpy as np
+
 from PIL import Image
 from PIL import ImageChops
 
@@ -8,19 +10,18 @@ from PIL import ImageChops
 model = tf.keras.applications.vgg16.VGG16(weights="imagenet", include_top=True)
 
 # Load a sample image (or multiple ones)
-img = tf.keras.preprocessing.image.load_img("images/cat.jpg", target_size=(224, 224))
+img = tf.keras.preprocessing.image.load_img("images/input_nn/cat.jpg", target_size=(224, 224))
 img = tf.keras.preprocessing.image.img_to_array(img)
 data = ([img], None)
 
 # Start explainer
 explainer = GradCAM()
-grid = explainer.explain(data, model, class_index=281)  # 281 is the tabby cat index in ImageNet
+actual = explainer.explain(data, model, class_index=281)  # 281 is the tabby cat index in ImageNet
 
-actual = Image.fromarray(grid)
-expected = Image.open('images/expected.png')
+explainer.save(actual, '.', 'src/cat.png')
 
-diff = ImageChops.difference(actual, expected)
+expected = np.asarray(Image.open('images/output_nn/cat.png'))
 
-assert diff.getbbox() == False, "shit happens!" 
+assert np.array_equal(actual, expected), "shit happens!" 
 
-print("Test is passed.")
+print("Tests are passed!")
