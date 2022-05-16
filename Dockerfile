3f735a29f40d12b33b1ea5919c9732aa65c91184
RodionovDenis <denisrodionov18@gmail.com>
2022-05-16 04:01:15 +0300

    changes

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,15 @@ RUN apt-get update && \
     apt-get upgrade -y && \
     apt-get install -y wget mc tmux nano build-essential rsync libgl1 python3-pip
 
+
+
+RUN pip install tf-explain
+RUN pip install tensorflow==2.6.0
+RUN pip install keras==2.6.0
+RUN pip install opencv-python
+RUN pip install Pillow
+
 COPY test.py /test.py
+COPY images /images
 
 CMD python3 test.py
--- test.py
+++ test.py
@@ -1,1 +1,26 @@
-print("hello docker!")
+import tensorflow as tf
+from tf_explain.core.grad_cam import GradCAM
+from PIL import Image
+from PIL import ImageChops
+
+
+# Load pretrained model or your own
+model = tf.keras.applications.vgg16.VGG16(weights="imagenet", include_top=True)
+
+# Load a sample image (or multiple ones)
+img = tf.keras.preprocessing.image.load_img("images/cat.jpg", target_size=(224, 224))
+img = tf.keras.preprocessing.image.img_to_array(img)
+data = ([img], None)
+
+# Start explainer
+explainer = GradCAM()
+grid = explainer.explain(data, model, class_index=281)  # 281 is the tabby cat index in ImageNet
+
+actual = Image.fromarray(grid)
+expected = Image.open('images/expected.png')
+
+diff = ImageChops.difference(actual, expected)
+
+assert diff.getbbox() == False, "shit happens!" 
+
+print("Test is passed.")
