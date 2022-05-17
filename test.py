03f7c803978bd1ebef25424ab838eff804f3ef7e
Denis Rodionov <denisrodionov5@gmail.com>
2022-05-17 14:52:58 +0300

    add dog and shark

--- test.py
+++ test.py
@@ -1,27 +1,26 @@
 import tensorflow as tf
-from tf_explain.core.grad_cam import GradCAM
 import numpy as np
 
+from tf_explain.core.grad_cam import GradCAM
 from PIL import Image
-from PIL import ImageChops
-
 
 # Load pretrained model or your own
 model = tf.keras.applications.vgg16.VGG16(weights="imagenet", include_top=True)
 
 # Load a sample image (or multiple ones)
-img = tf.keras.preprocessing.image.load_img("images/input_nn/cat.jpg", target_size=(224, 224))
+img = tf.keras.preprocessing.image.load_img("images/input_nn/shark.jpg", target_size=(224, 224))
 img = tf.keras.preprocessing.image.img_to_array(img)
 data = ([img], None)
 
 # Start explainer
 explainer = GradCAM()
-actual = explainer.explain(data, model, class_index=281)  # 281 is the tabby cat index in ImageNet
+actual = explainer.explain(data, model, class_index=3)  # 281 is the tabby cat index in ImageNet
+# 3 - shark, 180 - dog
 
-explainer.save(actual, '.', 'src/cat.png')
+explainer.save(actual, '.', 'src/shark.png')
 
-expected = np.asarray(Image.open('images/output_nn/cat.png'))
+#expected = np.asarray(Image.open('images/output_nn/cat.png'))
 
-assert np.array_equal(actual, expected), "shit happens!" 
+#assert np.array_equal(actual, expected), "shit happens!" 
 
-print("Tests are passed!")
+#print("Tests are passed!")
